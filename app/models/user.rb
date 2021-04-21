b70c091c75560b87fa82b3a8477255e1780a8849
Vincenzo <vinzgameslol@gmail.com>
2021-04-21 18:03:07 +0200

    Built User and Post model, validated and migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+has_many :posts 
+validates :name, presence: true, length: { in: 3..20 }, uniqueness: true
+validates :email, presence: true, length: { in: 3..20 }, uniqueness: true
 end
